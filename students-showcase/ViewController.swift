13e163b271a9cea15c29826414a1d650ee585b63
Андрей Букша <andreybuksha@icloud.com>
2015-11-22 13:34:09 +0300

    Initial Commit

---
+++ students-showcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  students-showcase
+//
+//  Created by Андрей Букша on 22.11.15.
+//  Copyright © 2015 Андрей Букша. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
